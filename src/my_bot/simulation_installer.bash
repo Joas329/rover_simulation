8d5da1580d6408bb5c7d3e414d48ddd6d7fbcaca
Joas329 <joaquinphilco@hotmail.com>
2023-06-28 00:17:14 -0400

    adding simulation code

---
+++ src/my_bot/simulation_installer.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt install gazebo
+
+#installing xacro, Gazebo, Ros2_control
+
+sudo apt install ros-humble-xacro
+
+sudo apt install ros-humble-gazebo-ros-pkgs
+
+sudo apt install ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-gazebo-ros2-control
+
+
+#sourcing!
+
+source /opt/ros/humble/setup.bash
