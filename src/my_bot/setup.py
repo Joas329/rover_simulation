c150b9f89aafb65c0add2f1b37ecd2ce55b8fcc3
Joas329 <joaquinphilco@hotmail.com>
2023-06-27 23:53:56 -0400

    adding package

---
+++ src/my_bot/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'my_bot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='joas329',
+    maintainer_email='joaquinphilco@hotmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
