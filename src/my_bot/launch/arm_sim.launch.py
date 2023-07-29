b19a91a57587b1a3de44ef3d9aeb22ebea9421d3
Joas329 <joaquinphilco@hotmail.com>
2023-07-28 23:11:44 -0400

    testing

--- src/my_bot/launch/arm_sim.launch.py
+++ src/my_bot/launch/arm_sim.launch.py
@@ -65,18 +65,18 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
-        # RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=spawn_entity,
-        #         on_exit=[load_joint_state_controller],
-        #     )
-        # ),
-        # RegisterEventHandler(
-        #     event_handler=OnProcessExit(
-        #         target_action=load_joint_state_controller,
-        #         on_exit=[load_arm_controller],
-        #     )
-        # ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_arm_controller],
+            )
+        ),
         gazebo,
         node_robot_state_publisher,
         spawn_entity,
