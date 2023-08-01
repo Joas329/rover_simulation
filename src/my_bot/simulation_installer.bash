c2c677fb3d29c034c3cd405db0f025a0b47e988b
Joas329 <joaquinphilco@hotmail.com>
2023-08-01 08:00:47 -0400

    new laptop

--- src/my_bot/simulation_installer.bash
+++ src/my_bot/simulation_installer.bash
@@ -11,6 +11,8 @@ sudo apt install ros-humble-gazebo-ros-pkgs
 sudo apt install ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-gazebo-ros2-control
 
 
+sudo apt install ros-humble-moveit
+
 #sourcing!
 
 source /opt/ros/humble/setup.bash
